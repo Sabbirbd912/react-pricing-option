1c5dfab1ab5ed504b2111a989d9c58404dad5d25
Sabbir Ahmed <sabbirbd912@gmail.com>
2026-03-17 13:22:54 +0600

    react+tailwind+daisyui+googlefont

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import "./App.css";
+import Hero from "./components/DaisyUI/Hero";
+import Navbar from "./components/DaisyUI/Navbar";
+
+function App() {
+  return (
+    <>
+    <header>
+      <Navbar></Navbar>
+    </header>
+    <main>
+      <Hero></Hero>
+    </main>
+
+
+
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/DaisyUI/Navbar.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+
+const Navbar = () => {
+    const navLinks = [
+        {
+            id: 1,
+            name: "Home",
+            path: "/"
+        },
+        {
+            id: 2,
+            name: "Analytics",
+            path: "/dashboard/analytics"
+        },
+        {
+            id: 3,
+            name: "Projects",
+            path: "/projects"
+        },
+        {
+            id: 4,
+            name: "Team",
+            path: "/settings/team"
+        },
+        {
+            id: 5,
+            name: "Profile",
+            path: "/user/profile"
+        }
+    ];
+  return (
+    <nav>
+        <ul>
+            {
+                navLinks.map(route=> <li>
+                    <a href={route.path}>{route.name}</a>
+                </li>)
+            }
+        </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
